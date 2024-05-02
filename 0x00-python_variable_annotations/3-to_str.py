6bfb6220524c9fb5e9e24224429c297ce683a789
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-05-02 19:56:12 +0200

    Convert a float to its string representation

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function to_str which takes a float n as
+argument and returns the string representation of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """Convert a float to its string representation."""
+    return str(n)
