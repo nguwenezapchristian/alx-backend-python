be5f8de2dd9ca7761febbeeb444cace000aa288b
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-05-02 20:12:42 +0200

    Variable Definitions

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This script defines and annotates the following variables:
+- a: An integer with a value of 1
+- pi: A float with a value of 3.14
+- i_understand_annotations: A boolean with a value of True
+- school: A string with a value of "Holberton"
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
