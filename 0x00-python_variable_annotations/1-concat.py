7cb8de32d29d7b1f2ef5e8a678205a60ed1cb01b
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-05-02 19:18:34 +0200

    concat  two strings

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -7,4 +7,4 @@ a string str2 as arguments and returns a concatenated string
 
 def concat(str1: str, str2: str) -> str:
     """ This func concatenate two strings """
-    return str1 + " " + str2
+    return str1 + str2
