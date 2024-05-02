80e8ae48fe0eaaf5e87b3ed5523ac4b5e79e7598
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-05-02 19:49:54 +0200

    return an int

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+ a type-annotated function floor which takes a float n as
+ argument and returns the floor of the float
+"""
+
+
+def floor(n: float) -> int:
+    """ emove the decimal part of a float and convert it to an integer """
+    return int(n)
