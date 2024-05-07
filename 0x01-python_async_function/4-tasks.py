6ef365031ab538a841f42ce163acc2f07b4fffbe
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-05-07 20:14:34 +0200

    returns A list of all the delays

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Take the code from wait_n and alter it into a new function task_wait_n.
+The code is nearly identical to wait_n except task_wait_random is being called
+"""
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Spawn wait_random n times with the specified max_delay.
+
+    Args:
+        n (int): The number of times to call wait_random.
+        max_delay (int): The maximum delay for each call to wait_random.
+
+    Returns:
+        List[float]: A list of all the delays (float values) in ascending
+        order.
+    """
+    delays = []
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+
+    return delays
