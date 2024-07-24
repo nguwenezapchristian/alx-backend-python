baca46e9d9c7337440afefc69a5896cf10376dec
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-07-24 15:20:25 +0200

    fixed issue for type annotations for lst and the return type

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -7,9 +7,11 @@ and an integer factor as arguments and returns a tuple
 from typing import List, Tuple
 
 
-def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+def zoom_array(
+        lst: Tuple[int, ...],
+        factor: int = 2) -> Tuple[int, ...]:
     """Zoom array by repeating each item factor times"""
-    zoomed_in: List = [
+    zoomed_in: List[int] = [
         item for item in lst
         for _ in range(factor)
     ]
