08820272921c52b079c9b7b6a3a678fe59eeb4e5
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-07-24 14:56:08 +0200

    Type Checking

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+a type-annotated function zoom_array that takes a list lst
+and an integer factor as arguments and returns a list
+"""
+
+from typing import List, Tuple
+
+
+def zoom_array(lst: List, factor: int = 2) -> List:
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
